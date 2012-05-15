f112c42b89dfb93c448f3d4b2ea1a5c3a201f26b
Alexey Klyukin <alexk@commandprompt.com>
2012-05-15 17:28:01 +0300

    Add separate functions to fetch the pgdata directory and directories assigned to tablespaces.

--- cmd_standby.sql
+++ cmd_standby.sql
@@ -7,6 +7,14 @@ CREATE OR REPLACE FUNCTION cmd_get_data_dirs() RETURNS SETOF TEXT AS $$
 $$ LANGUAGE 'SQL';
 COMMENT ON FUNCTION cmd_get_data_dirs() IS 'Returns data paths. The text input is for later when we have to determine between > 8.3';
 
+CREATE OR REPLACE FUNCTION cmd_get_pgdata() RETURNS TEXT AS $$
+	SELECT setting FROM pg_catalog.pg_settings WHERE name='data_directory';
+$$ LANGUAGE 'SQL' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION cmd_get_tablespaces() RETURNS SETOF TEXT AS $$
+	SELECT DISTINCT spclocation FROM pg_catalog.pg_tablespace WHERE spclocation IS NOT NULL;
+$$ LANGUAGE 'SQL' STABLE
+
 CREATE OR REPLACE FUNCTION cmd_pg_start_backup() RETURNS INT AS $$
    SELECT pg_start_backup('base_backup');
    SELECT 1;
